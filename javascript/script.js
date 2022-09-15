c9c4d58b36fab6bbf9b47be3834671cbdd3a7bdb
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-09-15 07:36:46 +0500

    Update javascript files directory

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,4 @@
 window.onload = () => {
-  const barMenu = document.querySelector('.bar-menu');
-  const popUp = document.querySelector('.pop-up');
-  const close = document.querySelector('.close');
-  const mainMenu = document.querySelector('.main-menu');
-
-  barMenu.addEventListener('click', () => {
-    popUp.classList.remove('hidden');
-  });
-
-  close.addEventListener('click', () => {
-    popUp.classList.remove('hidden');
-  });
-
-  mainMenu.addEventListener('click', () => {
-    popUp.classList.add('hidden');
-  });
-
   const featuredSpeakers = [
     {
       src: './images/speakers-images/speaker_01.png',
